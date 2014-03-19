897630b642a2a4d78a15438c528dba6cdc7d928f
Leonardo.Z <glasslion@gmail.com>
2014-03-19 09:32:20 +0800

    Setup the official plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+import os
+import sys
+
+BASE_DIR = os.path.dirname(__file__)
+
+# Clone the official plugin repo to the `official_plugins` dir
+# (https://github.com/getpelican/pelican-plugins)
+sys.path.append(os.path.join(BASE_DIR, "official_plugins"))
+
+import summary
 
 AUTHOR = u'Leonardo Zhou'
 SITENAME = u'\u7ffc\u56fe\u5357'
@@ -29,6 +39,8 @@ SOCIAL = (
     ('stack-overflow', 'http://stackoverflow.com/users/1093020/leonardo-z'),
 )
 
+PLUGINS = [summary]
+
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
@@ -52,3 +64,4 @@ PAGINATED_DIRECT_TEMPLATES = ('blog-index',)
 POST_LIMIT = 10
 STOCK_PHOTO = 'http://ww3.sinaimg.cn/large/6c3391c1gw1eejqmtombwj212w0gfte6.jpg'
 BLOG_LOGO = 'http://wing2south.qiniudn.com/leo-logo.png'
+
